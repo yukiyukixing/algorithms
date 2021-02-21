3d174b76bc9981ec368148b6ab38f33f37c67214
wudong <wudong@uinnova.com>
2021-02-21 21:22:20 +0800

    feat: 滑动窗口🏍🏍🏍✔✔

--- 2.数组/3.可获得的最大点数/index.js
+++ 2.数组/3.可获得的最大点数/index.js
@@ -1,21 +1,24 @@
 // 1.滑动窗口法
-const cardPoints = [1, 2, 3, 4, 5, 6, 1], k = 3;
+const cardPoints = [2, 2, 2], k = 2;
 var maxScore = function (cardPoints, k) {
-    let n = cardPoints.length;
-    windowLength = n - 3;
+    const n = cardPoints.length;
+    // 创建窗口长度
+    const windowLength = n - k;
     let sum = 0;
     for (let i = 0; i < windowLength; i++) {
         sum += cardPoints[i]
     }
     let minSum = sum;
-    for (let i = windowLength; i < n; i++) {
-
-
+    for (let i = windowLength; i < n; ++i) {
+        sum += cardPoints[i] - cardPoints[i - windowLength];
+        minSum = Math.min(minSum, sum);
     }
-    sum(tempArray)
+    return sumArray(cardPoints) - minSum
 };
+console.log(maxScore(cardPoints, k));
 
-function sum(array) {
+// 求和的方法
+function sumArray(array) {
     return array.reduce((acc, cur) => {
         return acc += cur
     }, 0)
